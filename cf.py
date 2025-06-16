355c31fd4758c331a4f4787a8bde7d97b98315e2
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-06-16 13:23:44 -0400

    Probability calculation

--- cf.py
+++ cf.py
@@ -1,9 +1,10 @@
 import random
 streakCounter = 0
-streakChecker = 0
-coinList = []
 
-for experiment in range (10000): 
+for experiment in range (10000):
+    streakChecker = 0 # streak checker and coin list is reset at the start of each loop
+    coinList = [] 
+
     for coinFlip in range (100):
         if random.randint(0,1) == 0:
             coinList.append("Heads")
@@ -14,12 +15,10 @@ for experiment in range (10000):
         if len(coinList) > 1 and coinList[-1] == coinList[-2]:
             streakChecker += 1
             if streakChecker == 5:
-                streakChecker = 0
                 streakCounter += 1
+                break
         else:
             streakChecker = 0
 
 print(streakCounter)
-
-# 100 CF will be run 10,000 times
-# How often will a streak of six heads or six tails come up?
+print(f'Chance of streak: %s%%' % (streakCounter / 100))
